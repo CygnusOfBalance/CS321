bb4cba03fb028a2b3053739a73fadf0ec6844af7
Holden <h.malinchock@verizon.net>
2019-10-31 14:33:07 -0400

    Installing AXIOS

--- web-app/src/main.js
+++ web-app/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import axios from 'vue-axios'
+import axios from './plugins/axios';
 //import loginPage from './components/loginPage';
 
 new Vue({
