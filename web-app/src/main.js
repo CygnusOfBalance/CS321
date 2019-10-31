51cb71e3fabebfa95fb0a0b85c790178b0cf942b
Holden <h.malinchock@verizon.net>
2019-10-31 13:45:27 -0400

    Adding axios to dev

--- web-app/src/main.js
+++ web-app/src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import axios from 'vue-axios'
 //import loginPage from './components/loginPage';
 
 new Vue({
+  axios, 
   vuetify,
   router,
   render: h => h(App)
