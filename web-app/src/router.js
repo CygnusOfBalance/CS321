540fe5febfc164e0e724f07aaddc4a506d87b12c
Holden <h.malinchock@verizon.net>
2019-11-04 15:30:04 -0500

    Fixing router, addeding calander component to the componentes

--- web-app/src/router.js
+++ web-app/src/router.js
@@ -23,7 +23,8 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" './views/About.vue')
     },*/
     {
-      path: '/create-calendar',
+      path: '/',
+      name : 'create-calendar',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
